2858a9c507da3cdba8688edee3a6882015cfb366
Jason Tang <jason@upbound.io>
2025-07-23 15:04:54 -0400

    add basic plumbing for provider startup checks.
    Signed-off-by: Jason Tang <jason@upbound.io>

--- cmd/provider/monolith/zz_main.go
+++ cmd/provider/monolith/zz_main.go
@@ -39,6 +39,7 @@ import (
 	"github.com/upbound/provider-gcp-beta/apis/v1alpha1"
 	"github.com/upbound/provider-gcp-beta/config"
 	resolverapis "github.com/upbound/provider-gcp-beta/internal/apis"
+	"github.com/upbound/provider-gcp-beta/internal/bootcheck"
 	"github.com/upbound/provider-gcp-beta/internal/clients"
 	"github.com/upbound/provider-gcp-beta/internal/controller"
 	"github.com/upbound/provider-gcp-beta/internal/features"
@@ -56,6 +57,13 @@ func deprecationAction(flagName string) kingpin.Action {
 		_, err := fmt.Fprintf(os.Stderr, "warning: Command-line flag %q is deprecated and no longer used. It will be removed in a future release. Please remove it from all of your configurations (ControllerConfigs, etc.).\n", flagName)
 		kingpin.FatalIfError(err, "Failed to print the deprecation notice.")
 		return nil
+	}
+}
+
+func init() {
+	err := bootcheck.CheckEnv()
+	if err != nil {
+		log.Fatalf("bootcheck failed. provider will not be started: %v", err)
 	}
 }
 
